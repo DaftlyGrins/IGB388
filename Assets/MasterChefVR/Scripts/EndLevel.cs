9a42542e02352492ef5d802d8162d252f31bde4e
Vinnie-C-Bruv <82975078+Vinnie-C-Bruv@users.noreply.github.com>
2024-05-16 12:42:55 +1000

    retexture

---
+++ Assets/MasterChefVR/Scripts/EndLevel.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLevel : MonoBehaviour
+{
+    public GameObject meal;
+    public GameObject noGameOver;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "plate")
+        {
+            if(other.transform.childCount == 6)
+            {
+                noGameOver.GetComponent<IncreasingTemp>().enabled = false;
+                meal = other.transform.gameObject;
+                meal.GetComponent<Plate>().gradingPlate();
+                Debug.Log("yippee");
+            }
+            else
+            {
+                Debug.Log("swagmode");
+            }
+        }
+
+    }
+}
